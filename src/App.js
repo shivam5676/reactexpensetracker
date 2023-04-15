3307449b710990d37091ad6954c26deb3f14c449
shivam5676 <ssultanpura9@gmail.com>
2023-04-15 11:23:27 -0700

    my first app using react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Expense from "./components/Expense";
+
+function App() {
+    return ( <
+        div >
+        <
+        h2 > Let 's get started!</h2> <Expense> </Expense>{" "} <
+        /div>
+    );
+}
+
+export default App;
---
+++ src/components/Expense.js
@@ -0,0 +1,12 @@
+function Expense() {
+    return ( < div > < h3 > expense item < /h3> <
+        h3 > Food rs 10 < /h3> <
+        h3 > petrol rs 100 < /h3> <
+        h3 > < /h3> <
+        h3 > movies rs 200 < /h3>
+
+        <
+        /div> );
+    }
+
+    export default Expense;
