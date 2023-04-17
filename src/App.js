0e37262fa050008116e0582fa5c37a2153436a42
shivam5676 <ssultanpura9@gmail.com>
2023-04-16 23:37:54 -0700

    my file got a some aditioal features like props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import Expense from "./components/Expense";
 
 function App() {
-    return ( <
-        div >
-        <
-        h2 > Let 's get started!</h2> <Expense> </Expense>{" "} <
-        /div>
-    );
+  const expenses = [
+    {
+      expense: "expense item",
+      food: "food",
+      petrol: "petrol rs 1000",
+      movie: "movie at rs 2000",
+      location: "Location of expenditure",
+    },
+    {
+      expense: "expense item",
+      food: "food",
+      petrol: "petrol rs 1000",
+      movie: "movie at rs 2000",
+      location: "Location of expenditure",
+    },
+    {
+      expense: "expense item",
+      food: "food",
+      petrol: "petrol rs 1000",
+      movie: "movie at rs 2000",
+      location: "Location of expenditure",
+    },
+    {
+      expense: "expense item",
+      food: "food",
+      petrol: "petrol rs 1000",
+      movie: "movie at rs 2000",
+      location: "Location of expenditure",
+    },
+  ];
+
+  return (
+    <div>
+      <h2> Let 's get started!</h2>
+      {/* if we want to run javascript method in jsx /react we have to add that method or function inside a curly braces see this map function we have added an curly braces before method function so that it can works without any error ,expenseitem means current item of array(Expenses array) and index means current item index each time both will be updated automatically*/}
+      {expenses.map((expenseitem, i) => (
+        <Expense
+          expense={expenses[i].expense}
+          food={expenses[i].food}
+          petrol={expenses[i].petrol}
+          movie={expenses[i].movies}
+          location={expenses[i].location}
+        ></Expense>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,12 +1,16 @@
-function Expense() {
-    return ( < div > < h3 > expense item < /h3> <
-        h3 > Food rs 10 < /h3> <
-        h3 > petrol rs 100 < /h3> <
-        h3 > < /h3> <
-        h3 > movies rs 200 < /h3>
+function Expense(props) {
+  
+  
+  return (
+  
+    <div>
+      <h3> {props.expense} </h3>
+      <h3> {props.food} </h3>
+      <h3> {props.petrol}</h3>
+      <h3> {props.movie} </h3>
+      <h3>{props.location}</h3>
+    </div>
+  );
+}
 
-        <
-        /div> );
-    }
-
-    export default Expense;
+export default Expense;
